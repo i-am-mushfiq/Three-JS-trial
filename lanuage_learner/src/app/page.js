85dfe910d32be9aae92746b875c2d84b043d6a89
i-am-mushfiq <mushfique2@iut-dhaka.edu>
2024-03-22 08:47:07 +0600

    new 2

--- lanuage_learner/src/app/page.js
+++ lanuage_learner/src/app/page.js
@@ -1,6 +1,4 @@
-import { Experience } from '@/components/Experience'
 import { Computer } from '@/components/Computer'
-import { Overlay } from '@/components/Overlay'
 import Image from 'next/image'
 import Head from 'next/head'
 
