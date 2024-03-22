a86c0826fd4e4e2bc966c70a55164744dc30a635
i-am-mushfiq <mushfique2@iut-dhaka.edu>
2024-03-22 08:36:18 +0600

    new 2

--- lanuage_learner/src/app/page.js
+++ lanuage_learner/src/app/page.js
@@ -2,10 +2,15 @@ import { Experience } from '@/components/Experience'
 import { Computer } from '@/components/Computer'
 import { Overlay } from '@/components/Overlay'
 import Image from 'next/image'
+import Head from 'next/head'
+
 
 export default function Home() {
   return (
     <main className='h-screen min-h-screen'>
+      <Head>
+        <title>iammushfiq</title>
+      </Head>
       < Computer/>
     </main>
   )
